56c4fb7cb0a2a20dce18b0dbcb9b210c44ff62d1
D-GU <dgodomg@gmail.com>
2025-05-20 13:47:42 +0300

    Intermediate changes: v2

---
+++ services/auth/models/user.py
@@ -0,0 +1,29 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, DateTime
+from sqlalchemy.orm import relationship
+
+from app.backend.db import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    # Id fields
+    id = Column(Integer, primary_key=True, index=True)
+
+    # Credentials fields
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+
+    # Records fields
+    last_checked_patient = Column(Integer, default=0)
+    current_patient = Column(Integer, default=0)
+
+    # Relationship field
+    patient = relationship(
+        "Patient",
+        uselist=False,
+        single_parent=True,
+        back_populates="user"
+    )
