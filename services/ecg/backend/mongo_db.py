23fa452317ef7c3b40bf0862b314f2be461a1b0f
D-GU <dgodomg@gmail.com>
2025-05-30 13:08:29 +0300

    Changed docker compose file. Added nginx configuration file.

--- services/ecg/backend/mongo_db.py
+++ services/ecg/backend/mongo_db.py
@@ -1,12 +1,18 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
 from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from ..models.ecg import ECG
 
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "ecg"
+MONGO_URI = os.getenv(
+    "MONGO_URL",
+    default="mongodb://localhost:27017"
+)
+
+DB_NAME = os.getenv("MONGO_DB_NAME", default="ecg")
+
 
 async def init_mongo():
     client = AsyncIOMotorClient(MONGO_URI)
     await init_beanie(database=client[DB_NAME], document_models=[ECG])
-
--- services/user_meta/backend/db.py
+++ services/user_meta/backend/db.py
@@ -1,11 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import (
     create_async_engine, async_sessionmaker, AsyncSession
 )
-
 from sqlalchemy.orm import DeclarativeBase
 
+load_dotenv(".env")
+POSTGRES_URL = os.getenv(
+    "PSGR_URL",
+    default="postgresql+asyncpg://forest:1887@localhost:5432/markecg"
+)
+
 engine = create_async_engine(
-    'postgresql+asyncpg://forest:1887@localhost:5432/markecg',
+    POSTGRES_URL,
     echo=True
 )
 
