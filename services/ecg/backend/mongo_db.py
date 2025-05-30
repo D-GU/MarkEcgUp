490040fc05bdc503060a16894bb15ec9bc3f1679
D-GU <dgodomg@gmail.com>
2025-05-30 13:14:36 +0300

    Minor changes

--- services/ecg/backend/mongo_db.py
+++ services/ecg/backend/mongo_db.py
@@ -5,14 +5,14 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from ..models.ecg import ECG
 
-MONGO_URI = os.getenv(
-    "MONGO_URL",
+MONGO_URL = os.getenv(
+    "MONGODB_URL",
     default="mongodb://localhost:27017"
 )
 
-DB_NAME = os.getenv("MONGO_DB_NAME", default="ecg")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", default="ecg")
 
 
 async def init_mongo():
-    client = AsyncIOMotorClient(MONGO_URI)
-    await init_beanie(database=client[DB_NAME], document_models=[ECG])
+    client = AsyncIOMotorClient(MONGO_URL)
+    await init_beanie(database=client[MONGO_DB_NAME], document_models=[ECG])
